7a4a53bb0c5355cf16029156b48ead08c27dd805
XVincentX <vincenzo@apiary.io>
2016-07-29 18:04:44 +0200

    Create 3 separate functions per type and move the code inside it

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -7,10 +7,76 @@
 import * as path from 'path';
 import * as fs from 'fs';
 
-import { window, workspace, Disposable, ExtensionContext, commands, Uri } from 'vscode';
-import * as vscode from 'vscode';
-
+import { window, workspace, Disposable, ExtensionContext, commands, Uri, WorkspaceEdit, Position, ViewColumn, EndOfLine } from 'vscode';
 import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, TransportKind } from 'vscode-languageclient';
+
+function showError(err) {
+  window.showErrorMessage(err.message || err);
+}
+
+function registerCommands(client: LanguageClient, context: ExtensionContext) {
+  context.subscriptions.push(commands.registerTextEditorCommand('apiElements.parserOutput', (editor) => {
+    const statusBarDisposable = window.setStatusBarMessage("Parsing current document...");
+    client.sendRequest({ method: "parserOutput" }, editor.document.getText())
+      .then((result) => {
+        const stringifiedResult = JSON.stringify(result, null, 2);
+        const uri = Uri.parse(`untitled:${path.join(workspace.rootPath || context.extensionPath, "parseResult.json")}`);
+        return Promise.all([stringifiedResult, uri, workspace.openTextDocument(uri)]);
+      })
+      .then(([stringifiedResult, uri, textDocument]) => {
+        const edit = new WorkspaceEdit();
+        edit.insert(<Uri>uri, new Position(0, 0), <string>stringifiedResult);
+        return Promise.all([<any>textDocument, workspace.applyEdit(edit)]);
+      })
+      .then(([textDocument, editApplied]) => {
+        return window.showTextDocument(<any>textDocument, ViewColumn.One, false);
+      })
+      .then(() => {
+        statusBarDisposable.dispose();
+      })
+      .then(null, showError);
+  }));
+
+  context.subscriptions.push(commands.registerCommand('apiElements.apiary.fetchApi', () => {
+    commands.executeCommand('vscode.open', Uri.parse('https://login.apiary.io/tokens'));
+  }));
+
+  context.subscriptions.push(commands.registerCommand('apiElements.apiary.publishApi', () => {
+    commands.executeCommand('vscode.open', Uri.parse('https://login.apiary.io/tokens'));
+  }));
+
+  context.subscriptions.push(commands.registerCommand('apiElements.apiary.logout', () => {
+    commands.executeCommand('vscode.open', Uri.parse('https://login.apiary.io/tokens'));
+  }));
+}
+
+function registerNotifications(client: LanguageClient) {
+  client.onNotification({ method: "openUrl" }, (url) => {
+    commands.executeCommand("vscode.open", Uri.parse(<string>url));
+  });
+}
+
+function registerWindowEvents() {
+  window.onDidChangeActiveTextEditor((textEditor) => {
+
+    if (textEditor.document.languageId === 'API Blueprint') {
+
+      const adjustEditor = workspace.getConfiguration('apiElements').get('editor.adjustOptions');
+
+      if (adjustEditor === true) {
+        textEditor.options = {
+          insertSpaces: false,
+          tabSize: 4,
+        };
+
+        textEditor.edit((editBuilder) => {
+          editBuilder.setEndOfLine(EndOfLine.LF);
+        });
+      }
+    }
+  })
+
+}
 
 export function activate(context: ExtensionContext) {
   const serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
@@ -30,55 +96,10 @@ export function activate(context: ExtensionContext) {
 
   const client = new LanguageClient('Api Elements', serverOptions, clientOptions);
 
-  context.subscriptions.push(commands.registerTextEditorCommand('apiElements.parserOutput', (editor) => {
-    const statusBarDisposable = window.setStatusBarMessage("Parsing current document...");
-    client.sendRequest({ method: "parserOutput" }, editor.document.getText())
-      .then((result) => {
-        const stringifiedResult = JSON.stringify(result, null, 2);
-        const uri = Uri.parse(`untitled:${path.join(workspace.rootPath || context.extensionPath , "parseResult.json")}`);
-        return Promise.all([stringifiedResult, uri, workspace.openTextDocument(uri)]);
-      })
-      .then(([stringifiedResult, uri, textDocument]) => {
-        const edit = new vscode.WorkspaceEdit();
-        edit.insert(<Uri>uri, new vscode.Position(0, 0), <string>stringifiedResult);
-        return Promise.all([<any>textDocument, workspace.applyEdit(edit)]);
-      })
-      .then(([textDocument, editApplied]) => {
-        return window.showTextDocument(<any>textDocument, vscode.ViewColumn.One, false);
-      })
-      .then(() => {
-        statusBarDisposable.dispose();
-      })
-      .then(null, showError);
-  }));
-
-  client.onNotification({ method: "openUrl" }, (url) => {
-    commands.executeCommand("vscode.open", vscode.Uri.parse(<string>url));
-  });
-
-  window.onDidChangeActiveTextEditor((textEditor) => {
-
-    if (textEditor.document.languageId === 'API Blueprint') {
-
-      const adjustEditor = workspace.getConfiguration('apiElements').get('editor.adjustOptions');
-
-      if (adjustEditor === true) {
-        textEditor.options = {
-          insertSpaces: false,
-          tabSize: 4,
-        };
-
-        textEditor.edit((editBuilder) => {
-          editBuilder.setEndOfLine(vscode.EndOfLine.LF);
-        });
-
-      }
-    }
-  })
+  registerCommands(client, context);
+  registerNotifications(client);
+  registerWindowEvents();
 
   context.subscriptions.push(client.start());
 
-  function showError(err) {
-    window.showErrorMessage(err.message || err);
-  }
 }
