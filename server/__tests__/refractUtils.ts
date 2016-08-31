9452536fc6209341fe7842703fae861fe8c42f35
Vincenzo Chianese <vincenzo@apiary.io>
2016-08-31 22:50:02 +0200

    Updated git ignore and renamed test template

---
+++ server/__tests__/refractUtils.ts
@@ -0,0 +1,6 @@
+import * as refractUtils from '../src/refractUtils';
+const drafter = require('drafter.js');
+
+it('adds 5 - 1 to equal 4 in TypeScript', () => {
+    expect(3 + 1).toBe(4);
+});
--- server/__tests__/sum-test.ts
+++
@@ -1,6 +0,0 @@
-declare var it: Function;
-declare var expect: Function;
-
-it('adds 5 - 1 to equal 4 in TypeScript', () => {
-    expect(3 + 1).toBe(4);
-});
