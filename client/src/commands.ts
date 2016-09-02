7faa048b71a99e50684ff553fcdd1b25f179c21a
XVincentX <vincenzo@apiary.io>
2016-09-02 17:58:56 +0200

    Send more status bar messages when async things happens

--- client/src/commands.ts
+++ client/src/commands.ts
@@ -28,43 +28,48 @@ function selectApi(context: ExtensionContext) {
 }
 
 export function parseOutput(context: ExtensionContext, client: LanguageClient, editor: TextEditor) {
-  const statusBarDisposable = window.setStatusBarMessage('Parsing current document...');
-  client.sendRequest({ method: 'parserOutput' }, editor.document.getText())
-    .then(result => showUntitledWindow('parseResult.json', JSON.stringify(result, null, 2), context.extensionPath))
-    .then(() => statusBarDisposable.dispose())
-    .then(undefined, showMessage);
+  window.setStatusBarMessage(
+    'Parsing current document...',
+    client.sendRequest({ method: 'parserOutput' }, editor.document.getText())
+      .then(result => showUntitledWindow('parseResult.json', JSON.stringify(result, null, 2), context.extensionPath))
+      .then(undefined, showMessage)
+  );
 }
 
 export function fetchApi(context: ExtensionContext) {
-  return selectApi(context)
-    .then(([selectedApi, client]) => {
-      if (selectedApi === undefined) {
-        throw 0;
-      }
+  window.setStatusBarMessage('Fetching API from Apiary...',
+    selectApi(context)
+      .then(([selectedApi, client]) => {
+        if (selectedApi === undefined) {
+          throw 0;
+        }
 
-      return Promise.all([(<any>client).getApiCode((<any>selectedApi).label), (<any>selectedApi).label]);
-    })
-    .then(([res, apiName]): Thenable<any> => {
-      if (window.activeTextEditor === undefined)
-        return showUntitledWindow(`${apiName}.apib`, (<any>res).code, context.extensionPath)
-      return window.activeTextEditor.edit((builder) => {
-        const lastLine = window.activeTextEditor.document.lineCount;
-        const lastChar = window.activeTextEditor.document.lineAt(lastLine - 1).range.end.character;
-        builder.delete(new Range(0, 0, lastLine, lastChar));
-        builder.replace(new Position(0, 0), (<any>res).code);
-      });
-    })
-    .then(undefined, showMessage);
+        return Promise.all([(<any>client).getApiCode((<any>selectedApi).label), (<any>selectedApi).label]);
+      })
+      .then(([res, apiName]): Thenable<any> => {
+        if (window.activeTextEditor === undefined)
+          return showUntitledWindow(`${apiName}.apib`, (<any>res).code, context.extensionPath)
+        return window.activeTextEditor.edit((builder) => {
+          const lastLine = window.activeTextEditor.document.lineCount;
+          const lastChar = window.activeTextEditor.document.lineAt(lastLine - 1).range.end.character;
+          builder.delete(new Range(0, 0, lastLine, lastChar));
+          builder.replace(new Position(0, 0), (<any>res).code);
+        });
+      })
+      .then(undefined, showMessage)
+  );
 
 }
 
 export function publishApi(context: ExtensionContext, textEditor: TextEditor) {
-  selectApi(context)
-    .then(([selectedApi, client]) => {
-      return (<any>client).publishApi((<any>selectedApi).label, textEditor.document.getText(), '');
-    })
-    .then(() => window.showInformationMessage('API successuflly published on Apiary!'))
-    .then(undefined, showMessage);
+  window.setStatusBarMessage('Publishing API on Apiary...',
+    selectApi(context)
+      .then(([selectedApi, client]) => {
+        return (<any>client).publishApi((<any>selectedApi).label, textEditor.document.getText(), '');
+      })
+      .then(() => window.showInformationMessage('API successuflly published on Apiary!'))
+      .then(undefined, showMessage)
+  );
 }
 
 export function logout(context: ExtensionContext) {
